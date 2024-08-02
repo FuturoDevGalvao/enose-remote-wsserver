e13bdedb7e90f2dc8d254db5a35f35b10035f448
galvao.dev <inosuke567765@gmail.com>
2024-08-02 05:59:46 -0300

    feat: adição da classe de renderização

---
+++ app/dist/utils/View.js
@@ -0,0 +1,5 @@
+export default class View {
+    static render(data) {
+        return JSON.stringify({ data });
+    }
+}
---
+++ app/utils/View.ts
@@ -0,0 +1,5 @@
+export default class View {
+  public static render(data: any) {
+    return JSON.stringify({ data });
+  }
+}
