d06e288c6f02ae64c74bf79bda11b115459c1a7c
galvao.dev <inosuke567765@gmail.com>
2024-08-03 14:13:15 -0300

    feat: adicionadas as classes que representarão as entidades

---
+++ app/database/entities/Sensor.ts
@@ -0,0 +1,25 @@
+export default class Sensor {
+  private id: number;
+  private name: string;
+
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public getId(): number {
+    return this.id;
+  }
+
+  public setId(id: number): void {
+    this.id = id;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public setName(name: string): void {
+    this.name = name;
+  }
+}
---
+++ app/database/entities/User.ts
@@ -0,0 +1,72 @@
+export default class User {
+  private id: number;
+  private name: string;
+  private email: string;
+  private emailValidated: boolean;
+  private validationToken: string;
+  private createdAt: Date;
+
+  constructor(
+    id: number,
+    name: string,
+    email: string,
+    emailValidated: boolean,
+    validationToken: string,
+    createdAt: Date
+  ) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.emailValidated = emailValidated;
+    this.validationToken = validationToken;
+    this.createdAt = createdAt;
+  }
+
+  public getId(): number {
+    return this.id;
+  }
+
+  public setId(id: number) {
+    this.id = id;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public setName(name: string) {
+    this.name = name;
+  }
+
+  public getEmail(): string {
+    return this.email;
+  }
+
+  public setEmail(email: string) {
+    this.email = email;
+  }
+
+  public getEmailValidated(): boolean {
+    return this.emailValidated;
+  }
+
+  public setEmailValidated(emailValidated: boolean) {
+    this.emailValidated = emailValidated;
+  }
+
+  public getValidationToken(): string {
+    return this.validationToken;
+  }
+
+  public setValidationToken(validationToken: string) {
+    this.validationToken = validationToken;
+  }
+
+  public getCreatedAt(): Date {
+    return this.createdAt;
+  }
+
+  public setCreatedAt(createdAt: Date) {
+    this.createdAt = createdAt;
+  }
+}
